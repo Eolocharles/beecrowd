4f6b6ba4712071c246774ea53ecf49ab6e98883b
Éolo Charles <eolocharles@gmail.com>
2023-07-11 10:23:01 -0300

    1564, 1589 solved

---
+++ src/iniciante/problem1564/ThereWillBeCup.java
@@ -0,0 +1,24 @@
+package iniciante.problem1564;
+
+import java.util.Scanner;
+
+
+
+
+public class ThereWillBeCup {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    while(sc.hasNext()){
+         int n = sc.nextInt();
+        if( n == 0){
+            System.out.println("vai ter copa!");
+        } else if (n > 0 ) {
+            System.out.println("vai ter duas!");
+
+        }
+
+    }
+
+    }
+}
